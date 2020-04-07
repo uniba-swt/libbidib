1500c76f8076a89a86e04b2124592ceafd0374a4
Eugene <eyip002@users.noreply.github.com>
2020-04-07 15:05:09 +0200

    Update bidib_config_parser_train.c

--- src/parser/bidib_config_parser_train.c
+++ src/parser/bidib_config_parser_train.c
@@ -446,7 +446,7 @@ static bool bidib_config_parse_single_train(yaml_parser_t *parser) {
 							}
 							break;
                         case TRAIN_PERIPHERALS_VALUE:
-                            // error = true;
+						//	error = true;
                             break;
                         default:
                             break;
