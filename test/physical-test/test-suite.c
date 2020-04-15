bd1432caa3461d833dcfd112e7be90b77f8a5617
Christof <Christof@lehanka.de>
2020-04-15 11:35:10 +0200

    Made some changes to the test-suite
    Note: Dont use this branch

--- test/physical-test/test-suite.c
+++ test/physical-test/test-suite.c
@@ -1,4 +1,4 @@
-
+#include "test-suite.h"
 
 void logTestResult(Test_result *test, t_bidib_unified_accessory_state_query state, int i){
 		if (state.known) {
@@ -26,18 +26,96 @@ void logTestResult(Test_result *test, t_bidib_unified_accessory_state_query stat
             }
 	}
 
-t_bidib_id_list_query sortOutIds(t_bidib_id_list_query idQuery, sortOutIds soid ){
-		t_bidib_id_list_query iq;
 
-		for (int i = 0; i < idQuery.length; i++) {
-			for(int j = 0; j < soid.length; j++ )
-					if(strcmp(points.ids[i], soid.ids[j])){
+	void testSignal(t_bidib_id_list_query signals){
+        for (int i = 0; i < points.length; i++) {
 
-					}
+            printf("testing %s  \n", points.ids[i]);
+            bidib_set_signal(signals.ids[i], "on");
+
+            state = bidib_get_point_state(points.ids[i]);
+            logTestResult(test, state, i);
+
+        }
+
+        for (i = 0; i < points.length; i++) {
+
+            printf("testing %s  \n", points.ids[i]);
+            bidib_switch_point(points.ids[i], "normal");
+
+            state = bidib_get_point_state(points.ids[i]);
+            logTestResult(test, state, i);
+
+        }
+	}
+
+void testPointParalel(t_bidib_id_list_query points, Test_result* test){
+    for (int i = 0; i < points.length; i++) {
+
+        printf("testing %s  \n", points.ids[i]);
+        bidib_switch_point(points.ids[i], "reverse");
+
+        state = bidib_get_point_state(points.ids[i]);
+        logTestResult(test, state, i);
+
+    }
+
+    for (int i = 0; i < points.length; i++) {
+
+        printf("testing %s  \n", points.ids[i]);
+        bidib_switch_point(points.ids[i], "normal");
+
+        state = bidib_get_point_state(points.ids[i]);
+        logTestResult(test, state, i);
+
+    }
+
+}
+
+
+void testPointSerial(t_bidib_id_list_query points, Test_result* test){
+		for (int i = 0; i < points.length; i++) {
+            printf("testing %s  \n", points.ids[i]);
+			bidib_switch_point(points.ids[i], "reverse");
+			state = bidib_get_point_state(points.ids[i]);
+			logTestResult(test, state, i);
+
+			}
+
+		sleep(WAITINGTIME);
+		for (int i = 0; i < points.length; i++) {
+            printf("testing %s  \n", points.ids[i]);
+			bidib_switch_point(points.ids[i], "normal");
+			state = bidib_get_point_state(points.ids[i]);
+			logTestResult(test, state, i);
+
+			}
+
+		sleep(WAITINGTIME);
+	}
+
+
+	void driveTo( char* segment, int speed, char* train){
+		bidib_set_train_speed(train, speed, master);
+		while(1) {
+			pos = bidib_get_train_position(train);
+					sleep(1);
+			for (int i = 0; i < pos.length; i++) {
+				if (!strcmp(pos.segments[i], segment)) {
+						return;
 				}
+			}
+	}
 
-	void testSignal(t_bidib_id_list_query signals);
-
-	void testPoint(t_bidib_id_list_query points, Test_result* test);
-
-	void driveTo(t_bidib_train_position_query *pos, char** segment);
+  void driveToStop( char* segment, int speed, char* train){
+		bidib_set_train_speed(train, speed, master);
+		while(1) {
+			pos = bidib_get_train_position(train);
+					sleep(1);
+			for (int i = 0; i < pos.length; i++) {
+				if (!strcmp(pos.segments[i], segment)) {
+						bidib_set_train_speed(train, 0, master);
+						return;
+				}
+			}
+	}
--- test/physical-test/test-suite.h
+++ test/physical-test/test-suite.h
@@ -5,6 +5,10 @@
 #include <stdlib.h>
 #include <string.h>
 
+t_bidib_id_list_query points;
+t_bidib_id_list_query signals;
+t_bidib_unified_accessory_state_query state;
+t_bidib_train_position_query pos;
 
 //misc
 typedef struct {
@@ -14,14 +18,10 @@ typedef struct {
     int stateReached;
     int stateReachedVerified;
     int unknownState;
-
 } Point_result;
 
 typedef struct {
     Point_result* trackEquipment;
-    t_bidib_id_list_query points;
-    t_bidib_id_list_query signals;
-
 } Test_result;
 
 typedef struct {
@@ -29,12 +29,9 @@ typedef struct {
   size_t length;
 } sortOutIds;
 
-t_bidib_unified_accessory_state_query state;
-t_bidib_train_position_query pos;
-
-t_bidib_id_list_query sortOutIds(t_bidib_id_list_query idQuery, sortOutIds soid );
-
 void logTestResult(Test_result *test, t_bidib_unified_accessory_state_query state, int i);
 void testSignal(t_bidib_id_list_query signals);
-void testPoint(t_bidib_id_list_query points, Test_result* test);
-void driveTo(t_bidib_train_position_query *pos, char** segment);
+void testPointParalel(t_bidib_id_list_query points, Test_result* test);
+void driveTo(t_bidib_train_position_query *pos, char* segment, int speed, char* train);
+void driveToStop(t_bidib_train_position_query *pos, char* segment, int speed, char* train);
+void testPointSerial(t_bidib_id_list_query points, Test_result* test);
