cd11f0bee5db6b5ba0c416c9ae37ca1680a5f57d
Eugene <eyip002@users.noreply.github.com>
2020-10-07 15:04:07 +0200

    Update main.c

--- test/physical/main.c
+++ test/physical/main.c
@@ -56,7 +56,7 @@ int main(int argc, char ** argv) {
         return 0;
     }
 
-    if (bidib_start_serial("/dev/ttyUSB0", "../../../../swtbahn-cli/configurations/swtbahn-standard", 200)) {
+    if (bidib_start_serial("/dev/ttyUSB0", "../../swtbahn-cli/configurations/swtbahn-standard", 200)) {
         printf("testsuite: libbidib failed to start\n");
         return 0;
     }
