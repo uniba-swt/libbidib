fb513dd0c8e8d85baa5c2ec9600d418b890038c9
Bernhard Luedtke <rush4wot@gmail.com>
2022-12-02 15:07:23 +0100

    log timestamps when receiving certain types of messages

--- src/highlevel/bidib_highlevel_setter.c
+++ src/highlevel/bidib_highlevel_setter.c
@@ -409,6 +409,7 @@ int bidib_set_train_speed_internal(const char *train, int speed, const char *tra
 int bidib_set_train_speed(const char *train, int speed, const char *track_output) {
 	struct timespec tv;
 	clock_gettime(CLOCK_MONOTONIC, &tv);
+	printf("\nSet train speed %d: Start at %ld.%.9ld\n", speed, tv.tv_sec, tv.tv_nsec);
 	syslog_libbidib(LOG_NOTICE, "Set train speed %d: Start at %d.%.9ld", speed, tv.tv_sec, tv.tv_nsec);
 	int ret = 0;
 	pthread_rwlock_wrlock(&bidib_state_trains_rwlock);
