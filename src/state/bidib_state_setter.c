0f784e69ae6807a5144ca964b81c3932c321090c
Bernhard Luedtke <rush4wot@gmail.com>
2024-12-04 13:26:56 +0100

    remove ineffective format specifiers (%.9 has no effect on integers, only on floats/reals)

--- src/state/bidib_state_setter.c
+++ src/state/bidib_state_setter.c
@@ -562,13 +562,13 @@ void bidib_state_log_train_detect(bool detected, const t_bidib_dcc_address *cons
 		if (train_state == NULL) {
 			syslog_libbidib(LOG_NOTICE,
 			                "Segment: %s is being entered by: unknown train (0x%02x%02x) "
-			                "with %s orientation, at time %d.%.9ld",
+			                "with %s orientation, at time %d.%.ld",
 			                segment_state->id->str, dcc_address->addrh, dcc_address->addrl,
 			                dcc_address->type == 0 ? "left" : "right", tv.tv_sec, tv.tv_nsec);
 		} else {
 			syslog_libbidib(LOG_NOTICE,
 			                "Segment: %s is being entered by: %s with %s "
-			                "orientation, at time %d.%.9ld",
+			                "orientation, at time %d.%.ld",
 			                segment_state->id->str, train_state->id->str,
 			                train_state->orientation == BIDIB_TRAIN_ORIENTATION_LEFT ? "left" : "right",
 			                tv.tv_sec, tv.tv_nsec);
@@ -577,13 +577,13 @@ void bidib_state_log_train_detect(bool detected, const t_bidib_dcc_address *cons
 		if (train_state == NULL) {
 			syslog_libbidib(LOG_NOTICE,
 			                "Segment: %s is being exited by: unknown train (0x%02x%02x) "
-			                "with %s orientation, at time %d.%.9ld",
+			                "with %s orientation, at time %d.%.ld",
 			                segment_state->id->str, dcc_address->addrh, dcc_address->addrl,
 			                dcc_address->type == 0 ? "left" : "right", tv.tv_sec, tv.tv_nsec);
 		} else {
 			syslog_libbidib(LOG_NOTICE,
 			                "Segment: %s is being exited by: %s with %s "
-			                "orientation, at time %d.%.9ld",
+			                "orientation, at time %d.%.ld",
 			                segment_state->id->str, train_state->id->str,
 			                train_state->orientation == BIDIB_TRAIN_ORIENTATION_LEFT ? "left" : "right",
 			                tv.tv_sec, tv.tv_nsec);
