c051a09e1c99f40e64e309c7282e120dadc00fd7
Bernhard Luedtke <bernhard_luedtke@posteo.de>
2026-01-26 10:29:30 +0100

    fix syslog format arg mistake for state query occupancy

--- src/state/bidib_state.c
+++ src/state/bidib_state.c
@@ -250,7 +250,7 @@ void bidib_state_query_occupancy(void) {
 			}
 			syslog_libbidib(LOG_INFO, "Querying occupancy and train addresses for board %s "
 			                "(0x%02x 0x%02x 0x%02x 0x00)", 
-			                board_i->id, board_i->node_addr.top, 
+			                board_i->id->str, board_i->node_addr.top, 
 			                board_i->node_addr.sub, board_i->node_addr.subsub);
 			// the math ops for the third argument ensure it is divisible by 8.
 			bidib_send_bm_get_range(board_i->node_addr, 0, 
